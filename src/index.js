14400ecd352bcb75ff73831a313f4face8cab708
sdh8055 <sdhmdh112@gmail.com>
2020-10-01 11:09:11 +0530

    text editor using Draft.js

--- src/index.js
+++ src/index.js
@@ -1,79 +1,48 @@
-import React, {Component} from 'react';
-import { render } from 'react-dom';
-import {EditorState} from "draft-js";
-import {Editor} from "react-draft-wysiwyg";
-import './App.css'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "./App.css";
 
-
-
-function uploadImageCallBack(file) {
-  return new Promise(
-    (resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', 'https://api.imgur.com/3/image');
-      xhr.setRequestHeader('Authorization', 'Client-ID ##clientid###');
-      const data = new FormData();
-      data.append('image', file);
-      xhr.send(data);
-      xhr.addEventListener('load', () => {
-        const response = JSON.parse(xhr.responseText);
-        console.log(response)
-        resolve(response);
-      });
-      xhr.addEventListener('error', () => {
-        const error = JSON.parse(xhr.responseText);
-        console.log(error)
-        reject(error);
-      });
-    }
-  );
-}
-
-
-class EditorContainer extends Component{
-  constructor(props){
+class EditorContainer extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       editorState: EditorState.createEmpty(),
     };
   }
 
-  onEditorStateChange: Function = (editorState) => {
+  onEditorStateChange = (editorState) => {
     // console.log(editorState)
     this.setState({
       editorState,
     });
   };
 
-  render(){
+  render() {
     const { editorState } = this.state;
-    return <div className='editor'>
-      <Editor
-        editorState={editorState}
-        onEditorStateChange={this.onEditorStateChange}    
-        toolbar={{
-          inline: { inDropdown: true },
-          list: { inDropdown: true },
-          textAlign: { inDropdown: true },
-          link: { inDropdown: true },
-          history: { inDropdown: true },
-          image: { uploadCallback: uploadImageCallBack, alt: { present: true, mandatory: true } },
-        }}
-      />
-    </div>
+    return (
+      <div className="my-little-app">
+        <h1>Playing with Draft.js!</h1>
+        <div className="draft-editor-wrapper">
+          <Editor
+            editorState={editorState}
+            onEditorStateChange={this.onEditorStateChange}
+            toolbar={{
+              inline: { inDropdown: true },
+              list: { inDropdown: true },
+              textAlign: { inDropdown: true },
+              link: { inDropdown: true },
+              history: { inDropdown: true },
+            }}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
+const App = () => <EditorContainer />
+ 
 
-
-
-
-
-const App = () => (
-  <div>
-    <h2>React Wysiwyg Rich Editor Using Draft.js</h2>
-    <EditorContainer />
-  </div>
-);
-
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
